be282ce5cc15577f44b5197f2ef584f512f1c3db
Your Name <you@example.com>
2024-06-11 21:06:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Assignment 12 is going')
+})
+
+app.listen(port, () =>{
+    console.log(`Assignment 12 is going on ${port} `);
+})
